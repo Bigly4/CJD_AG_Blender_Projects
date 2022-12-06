cdf4191735ccb94f63e9d8e5896564327c60b38d
Bigly4 _ <39852768+Bigly4@users.noreply.github.com>
2022-12-06 19:09:15 +0000

    alle aufgaben bis auf make

--- .ccls-cache/@home@runner@Uebung3PT/factorial_factors.c
+++ .ccls-cache/@home@runner@Uebung3PT/factorial_factors.c
@@ -1,10 +1,29 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int factorial(int n) {
-  
-}
-int main(int argc, char *argv) {
-  
-  return -1;
+  int res = 1;
+  for (int i = 2; i <= n; i++) {
+    res *= i;
   }
+  return res;
+}
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    return -1;
+  }
+  int N = atoi(argv[1]);
+  int fac = factorial(N);
+
+  while (fac != 1) {
+    for (int i = 2; i <= N; i++) {
+      if (fac % i == 0) {
+        printf("%d\n", i);
+        fac /= i;
+        break;
+      }
+    }
+  }
+  return 0;
+}
